e5826f646a5dbbeff0623a6c1cde2e156da7df05
Vlad Melekhin <vmelekhin@wsoft.ru>
2020-08-17 18:55:58 +0300

    Initial commit

---
+++ src/routes/routes.js
@@ -0,0 +1,44 @@
+import DashboardLayout from "@/pages/Layout/DashboardLayout.vue";
+
+import Dashboard from "@/pages/Dashboard.vue";
+import UserProfile from "@/pages/UserProfile.vue";
+import TableList from "@/pages/TableList.vue";
+import Typography from "@/pages/Typography.vue";
+import Notifications from "@/pages/Notifications.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: DashboardLayout,
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        component: Dashboard
+      },
+      {
+        path: "user",
+        name: "User Profile",
+        component: UserProfile
+      },
+      {
+        path: "table",
+        name: "Table List",
+        component: TableList
+      },
+      {
+        path: "typography",
+        name: "Typography",
+        component: Typography
+      },
+      {
+        path: "notifications",
+        name: "Notifications",
+        component: Notifications
+      }
+    ]
+  }
+];
+
+export default routes;
